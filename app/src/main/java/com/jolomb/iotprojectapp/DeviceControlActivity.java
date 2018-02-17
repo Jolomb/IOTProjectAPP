f41ac89a747e69a48d19ac85ddba3a2c2a6dc859
User-PC\User <eyalrs21@gmail.com>
2018-02-17 13:19:14 +0200

    Verification works!

--- app/src/main/java/com/jolomb/iotprojectapp/DeviceControlActivity.java
+++ app/src/main/java/com/jolomb/iotprojectapp/DeviceControlActivity.java
@@ -40,10 +40,16 @@ import android.widget.SimpleExpandableListAdapter;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.security.KeyFactory;
+import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
+import java.security.Signature;
+import java.security.interfaces.RSAPublicKey;
+import java.security.spec.X509EncodedKeySpec;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.regex.Pattern;
 
 /**
  * For a given BLE device, this Activity provides the user interface to connect, display data,
@@ -56,11 +62,16 @@ public class DeviceControlActivity extends Activity {
     private enum LockState {
         WAITING_FOR_INPUT_BUFFER,
         WAITING_FOR_ON_BOARD_CLICK,
-        RESPONSE_READY
+        RESPONSE_READY,
+        SIGNATURE_DONE,
+        SIGNING_FAILED,
+        INCORRECT_KEY
     }
     private final static char REMOTE_WAITING_FOR_INPUT_CHAR = 'W';
     private final static char REMOTE_WAITING_FOR_ONBOARD_BUTTON_CHAR = 'P';
     private final static char REMOTE_LOCK_RESPONSE_READY_CHAR = 'R';
+    private final static String REMOTE_LOCK_DONE_STRING = "D";
+    private final static char REMOTE_SIGN_FAILED_CHAR = 'N';
 
     private final static String TAG = DeviceControlActivity.class.getSimpleName();
 
@@ -75,6 +86,9 @@ public class DeviceControlActivity extends Activity {
     // Specific char of the remote device
     private BluetoothGattCharacteristic mRemoteLockBufferChar;
     private BluetoothGattCharacteristic mRemoteSignedResponseBuffer;
+    private BluetoothGattCharacteristic mRemoteLockStateChar;
+
+    private final String SIGNATURE_ALGORITHM = "SHA256withRSA";
 
     private ArrayList<ArrayList<BluetoothGattCharacteristic>> mGattCharacteristics =
             new ArrayList<ArrayList<BluetoothGattCharacteristic>>();
@@ -84,10 +98,40 @@ public class DeviceControlActivity extends Activity {
     private final String LIST_UUID = "UUID";
 
     private final int CRYPTO_CHALLANGE_LENGTH = 16;
+    private final int CRYPTO_RESPONSE_LENGTH = 128;
     private byte mChallangeBytes[];
 
     private LockState mRemoteLockState;
     private TextView mRemoteLocakStateText;
+
+
+    private final byte PUBLIC_KEY_DER_PKCS8_BYTES[] = {
+            0x30
+            , (byte)0x81, (byte)0x9f, (byte)0x30, (byte)0x0d, (byte)0x06, (byte)0x09, (byte)0x2a
+            , (byte)0x86, (byte)0x48, (byte)0x86, (byte)0xf7, (byte)0x0d, (byte)0x01, (byte)0x01
+            , (byte)0x01, (byte)0x05, (byte)0x00, (byte)0x03, (byte)0x81, (byte)0x8d, (byte)0x00
+            , (byte)0x30, (byte)0x81, (byte)0x89, (byte)0x02, (byte)0x81, (byte)0x81, (byte)0x00
+            , (byte)0x93, (byte)0x6a, (byte)0x1e, (byte)0x8d, (byte)0x89, (byte)0xfa, (byte)0x30
+            , (byte)0xb1, (byte)0x76, (byte)0x6b, (byte)0xa5, (byte)0xa5, (byte)0x8f, (byte)0x47
+            , (byte)0xf2, (byte)0xc9, (byte)0x91, (byte)0x69, (byte)0x33, (byte)0x43, (byte)0xdd
+            , (byte)0x72, (byte)0xd8, (byte)0x20, (byte)0x6c, (byte)0xc4, (byte)0xa4, (byte)0x4c
+            , (byte)0x79, (byte)0xff, (byte)0x0a, (byte)0xda, (byte)0xe4, (byte)0xa3, (byte)0xad
+            , (byte)0xc5, (byte)0x94, (byte)0x9b, (byte)0xe8, (byte)0x45, (byte)0x08, (byte)0x33
+            , (byte)0x96, (byte)0x73, (byte)0x5d, (byte)0xf1, (byte)0xf9, (byte)0xc9, (byte)0x24
+            , (byte)0xd6, (byte)0xcc, (byte)0x2d, (byte)0xd5, (byte)0x13, (byte)0xd3, (byte)0xa2
+            , (byte)0x45, (byte)0x14, (byte)0x6d, (byte)0x9d, (byte)0xcb, (byte)0x2e, (byte)0xe3
+            , (byte)0xa6, (byte)0xf4, (byte)0x42, (byte)0x8a, (byte)0x74, (byte)0x69, (byte)0xbf
+            , (byte)0x8f, (byte)0x27, (byte)0x4a, (byte)0x37, (byte)0x24, (byte)0xb8, (byte)0x8c
+            , (byte)0x8b, (byte)0xcf, (byte)0xa6, (byte)0xf0, (byte)0x5b, (byte)0x9b, (byte)0x95
+            , (byte)0x6a, (byte)0x30, (byte)0xa6, (byte)0xf5, (byte)0xbd, (byte)0xac, (byte)0xab
+            , (byte)0x29, (byte)0xc1, (byte)0x29, (byte)0xbb, (byte)0x3a, (byte)0x94, (byte)0x6c
+            , (byte)0x47, (byte)0x05, (byte)0x07, (byte)0x12, (byte)0xee, (byte)0x4b, (byte)0xaa
+            , (byte)0xc5, (byte)0xd6, (byte)0x46, (byte)0x02, (byte)0xfd, (byte)0x67, (byte)0xd2
+            , (byte)0x76, (byte)0x13, (byte)0xf1, (byte)0x8f, (byte)0xeb, (byte)0x15, (byte)0x5f
+            , (byte)0x5b, (byte)0x4f, (byte)0x8b, (byte)0xd6, (byte)0xb4, (byte)0x02, (byte)0x91
+            , (byte)0xbd, (byte)0x91, (byte)0x02, (byte)0x03, (byte)0x01, (byte)0x00, (byte)0x01
+    };
+
 
     // Code to manage Service lifecycle.
     private final ServiceConnection mServiceConnection = new ServiceConnection() {
@@ -149,6 +193,10 @@ public class DeviceControlActivity extends Activity {
                                 case REMOTE_LOCK_RESPONSE_READY_CHAR:
                                     mRemoteLockState = LockState.RESPONSE_READY;
                                     break;
+                                case REMOTE_SIGN_FAILED_CHAR:
+                                    mRemoteLockState = LockState.SIGNING_FAILED;
+                                    break;
+
                             }
                             updateRemoteLockState(mRemoteLockState);
                         } catch (NumberFormatException ex) {
@@ -156,6 +204,36 @@ public class DeviceControlActivity extends Activity {
                             updateConnectionState(R.string.connected_non_compatible);
                         }
                     }
+                } else if (char_uuid.equals(SampleGattAttributes.CRYPTO_SIGNER_SIGNED_RESPONSE)) {
+                    try {
+                        Signature publicSignature = Signature.getInstance(SIGNATURE_ALGORITHM);
+                        X509EncodedKeySpec spec1 = new X509EncodedKeySpec(PUBLIC_KEY_DER_PKCS8_BYTES);
+                        KeyFactory kf1 = KeyFactory.getInstance("RSA");
+                        RSAPublicKey pubKey = (RSAPublicKey) kf1.generatePublic(spec1);
+                        publicSignature.initVerify(pubKey);
+                        publicSignature.update(mChallangeBytes, 0, CRYPTO_CHALLANGE_LENGTH);
+
+                        String elements[] = char_data.split(" ");
+                        byte byteElements[] = new byte[CRYPTO_RESPONSE_LENGTH];
+                        for (int i = 0; i < CRYPTO_RESPONSE_LENGTH; i++)
+                            byteElements[i] = (byte)Integer.parseInt(elements[i], 16);
+                        if (elements.length == CRYPTO_RESPONSE_LENGTH){
+                            if (publicSignature.verify(byteElements, 0, CRYPTO_RESPONSE_LENGTH)) {
+                                mRemoteLockState = LockState.SIGNATURE_DONE;
+                                updateRemoteLockState(mRemoteLockState);
+
+                                //Reset the remote board for another signature
+                                mBluetoothLeService.writeCharacteristic(mRemoteLockStateChar,
+                                        REMOTE_LOCK_DONE_STRING);
+                            } else {
+                                mRemoteLockState = LockState.INCORRECT_KEY;
+                                updateRemoteLockState(mRemoteLockState);
+                            }
+                        } else {
+                            mRemoteLockState = LockState.SIGNING_FAILED;
+                            updateRemoteLockState(mRemoteLockState);
+                        }
+                    } catch (Exception ex) { /* This can be solved at compilation time...*/ }
                 }
             }
         }
@@ -197,18 +275,32 @@ public class DeviceControlActivity extends Activity {
 
             @Override
             public void onClick(View v) {
-                Toast.makeText(DeviceControlActivity.this, "Generating the random Crypto Challange", Toast.LENGTH_LONG).show();
-                SecureRandom random = new SecureRandom();
-                DeviceControlActivity.this.mChallangeBytes = new byte[CRYPTO_CHALLANGE_LENGTH];
-                random.nextBytes(mChallangeBytes);
-                final String randomString = new String(mChallangeBytes);
 
-                // Write the challange we just created to the remote GATT char
-                DeviceControlActivity.this.mBluetoothLeService.writeCharacteristic(
-                        DeviceControlActivity.this.mRemoteLockBufferChar,
-                        randomString
-                );
+                if ( (mRemoteLockState == LockState.WAITING_FOR_INPUT_BUFFER) &&
+                        (mConnected) && (mRemoteLockBufferChar != null)) {
+                    Toast.makeText(DeviceControlActivity.this, "Generating the random Crypto Challange", Toast.LENGTH_LONG).show();
+                    SecureRandom random = new SecureRandom();
+                    DeviceControlActivity.this.mChallangeBytes = new byte[CRYPTO_CHALLANGE_LENGTH];
+                    random.nextBytes(mChallangeBytes);
+
+                    //final String randomString = new String(mChallangeBytes);
+                    //TODO: REMOVE THIS!
+                    final String randomString = "1111111111111111";
+                    for (int i = 0; i < CRYPTO_CHALLANGE_LENGTH; i++)
+                        mChallangeBytes[i] = '1';
+
+                    // Write the challange we just created to the remote GATT char
+                    DeviceControlActivity.this.mBluetoothLeService.writeCharacteristic(
+                            DeviceControlActivity.this.mRemoteLockBufferChar,
+                            randomString
+                    );
+                } else if(mRemoteLockState == LockState.RESPONSE_READY) {
+                    // Read the signed response from the remote BLE device
+                    Toast.makeText(DeviceControlActivity.this, "Verifiying the Response now!", Toast.LENGTH_LONG).show();
+                    DeviceControlActivity.this.mBluetoothLeService.readCharacteristic(mRemoteSignedResponseBuffer);
+                }
             }
+
         });
     }
 
@@ -271,13 +363,27 @@ public class DeviceControlActivity extends Activity {
                 switch (state) {
                     case WAITING_FOR_INPUT_BUFFER:
                         mRemoteLocakStateText.setText(R.string.waiting_for_user_click);
+                        ((ImageView)findViewById(R.id.lock_image)).setImageResource(R.drawable.unlocked_lock);
                         break;
                     case WAITING_FOR_ON_BOARD_CLICK:
                         mRemoteLocakStateText.setText(R.string.waiting_for_onboard_click);
+                        ((ImageView)findViewById(R.id.lock_image)).setImageResource(R.drawable.cogs);
                         break;
                     case RESPONSE_READY:
                         mRemoteLocakStateText.setText(R.string.lock_response_ready);
+                        ((ImageView)findViewById(R.id.lock_image)).setImageResource(R.drawable.question_mark);
                         break;
+                    case SIGNING_FAILED:
+                        mRemoteLocakStateText.setText(R.string.lock_failed);
+                        ((ImageView)findViewById(R.id.lock_image)).setImageResource(R.drawable.orange_error);
+                        break;
+                    case INCORRECT_KEY:
+                        mRemoteLocakStateText.setText(R.string.incorrect_key);
+                        ((ImageView)findViewById(R.id.lock_image)).setImageResource(R.drawable.access_denied);
+                        break;
+                    case SIGNATURE_DONE:
+                        mRemoteLocakStateText.setText(R.string.access_granted);
+                        ((ImageView)findViewById(R.id.lock_image)).setImageResource(R.drawable.access_granted);
                 }
             }
         });
@@ -356,14 +462,13 @@ public class DeviceControlActivity extends Activity {
                 if (uuid.equals(SampleGattAttributes.CRYPTO_SIGNER_RESPONSE_STATE)) {
                     mBluetoothLeService.readCharacteristic(gattCharacteristic);
                     mBluetoothLeService.setCharacteristicNotification(gattCharacteristic, true);
+                    mRemoteLockStateChar = gattCharacteristic;
                 } else if (uuid.equals(SampleGattAttributes.CRYPTO_SIGNER_CHALLANGE_INPUT)) {
                     // Hold this char aside for a while
                     mRemoteLockBufferChar = gattCharacteristic;
                 } else if (uuid.equals(SampleGattAttributes.CRYPTO_SIGNER_SIGNED_RESPONSE)) {
                     mRemoteSignedResponseBuffer = gattCharacteristic;
                 }
-
-
             }
             mGattCharacteristics.add(charas);
             gattCharacteristicData.add(gattCharacteristicGroupData);
